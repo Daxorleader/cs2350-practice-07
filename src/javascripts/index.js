0308d9894cc42be77ff1c6d6534fefeb32fbab95
Daxton <daxtonbutler@yahoo.com>
2020-03-26 15:17:37 -0600

    done

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,18 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+
+const disrand = function(){
+    let r = Math.random()
+    if(r<.5){
+        document.write('<div class="red box"></div>');
+    }else{
+        document.write('<div class="blue box"></div>');
+    }
+}
+
+for(let i = 0; i < 50; i++){
+    disrand();
+}
+
+alert(square(10));
